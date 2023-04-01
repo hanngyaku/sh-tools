e2a3765dee4284bb1c882ad5799d5bce42f12dfa
hanngyaku <chenganggui@foxmail.com>
2023-04-01 11:31:10 +0800

    修改批量重命名文件夹的脚本

--- rename/dir_rename.sh
+++ rename/dir_rename.sh
@@ -3,25 +3,26 @@
 # 检查参数是否正确
 if [ $# -ne 3 ]; then
     echo "Usage: $0 path file mode"
+    echo "需要传入3个参数：dir_rename.sh path reference_file splite_str"
     exit 1
 fi
 
 # 逐行读取文件内容，并对每行进行处理
 while read line; do
   # 提取角色名和文件夹名
-  old_name=$(echo $line | awk -F, '{print $1}')
+  old_name=$(echo $line | awk -F${3} '{print $1}')
  
   # echo $role_name
-  suf_name=$(echo $line | awk -F, '{print $2}')
+  new_name=$(echo $line | awk -F${3} '{print $2}')
 
   #new_folder_name=$(echo $line | awk -F, '{print $2$1}')
   # echo $folder_name
   # 构造新的文件夹名
-  new_folder_name="${suf_name}${old_name:5}"
-  echo $new_folder_name
+  # new_folder_name="${suf_name}${old_name:5}"
+  echo $new_name
   # echo $new_floder_name
   # 执行重命名操作
-  mv "$1/${old_name}" "$1/${new_folder_name}"
+  mv "$1/${old_name}" "$1/${new_name}"
   # echo " mv "$1/${old_name}" "$1/${new_folder_name}""
 
  
