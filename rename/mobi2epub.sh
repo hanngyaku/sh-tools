fb8c0ca8aeb9942b864811303be546282af53007
hanngyaku <chenganggui@foxmail.com>
2023-04-01 11:36:56 +0800

    整理修改文件类型以及类型的脚本

--- rename/mobi2epub.sh
+++ rename/mobi2epub.sh
@@ -8,8 +8,7 @@ files=$(find "${1}" -type f -name "*.mobi")
 for file in $files
 do
 	echo "${file/mobi/epub}"
-    ebook-convert "${file}" "${file/mobi/epub}"
-	#mogrify -format jpg -resize 25% ${img}
+	ebook-convert "${file}" "${file/mobi/epub}"	
 done
 
 IFS="$OIFS"
